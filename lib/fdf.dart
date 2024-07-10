4aeb95dd6665d28425a2944331a78822801ebf34
nadiafitriani <160827922+nadiafitriani@users.noreply.github.com>
2024-07-10 09:36:15 +0700

    Update fdf.dart
    Nadia Fitriani - Update fdf.dart

--- lib/fdf.dart
+++ lib/fdf.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter API Integration',
+      title: 'Flutter API INtegration',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
